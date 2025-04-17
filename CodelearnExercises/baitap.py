ff2883ac09b12674748c2c398cbbbac023769269
Nguyen Dang Hanh <hnhngyndng@gmail.com>
2025-04-17 16:32:17 +0700

    The  first time Using git

---
+++ CodelearnExercises/baitap.py
@@ -0,0 +1,16 @@
+#Hello
+def say_hello(name):
+    print("Hello", name)
+
+#Total
+def total(a,b):
+    print(a+b)
+
+#Operation
+def Operation(a,b):
+    print(f"{a} + {b} =",a+b)
+    print(f"{a} - {b} =",a-b)
+    print(f"{a} * {b} =",a*b)
+    print(f"{a} / {b} =",a/b)
+
+Operation(10,5)
